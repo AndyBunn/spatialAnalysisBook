5f37f203d4150fd1a4ba89a83e9667bc8cdbebf0
AndyBunn <bunna@wwu.edu>
2026-06-06 12:29:14 -0700

    more!

---
+++ rScripts/09xAsidePredictingNewData.R
@@ -0,0 +1,28 @@
+## ----message=FALSE------------------------------------------------------------
+library(tidyverse)
+
+
+## -----------------------------------------------------------------------------
+set.seed(42)
+n <- 50
+b0 <- 4
+b1 <- 0.6
+b2 <- 3
+originalDat <- data.frame(x1=rnorm(n), x2=rnorm(n), epsilon = rnorm(n,sd = 2))
+originalDat$y <- b0 + b1 * originalDat$x1 + b2 * originalDat$x2 + originalDat$epsilon
+
+
+## -----------------------------------------------------------------------------
+lm1 <- lm(y~x1 + x2, data = originalDat)
+summary(lm1)
+
+
+## -----------------------------------------------------------------------------
+newDat <- data.frame(x1=rnorm(n), x2=rnorm(n))
+head(newDat)
+
+
+## -----------------------------------------------------------------------------
+newDat$yhat <- predict(object = lm1, newdata = newDat)
+head(newDat)
+
